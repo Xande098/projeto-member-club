67b5b0f1f17dc558c58b61fed6b81e4edbbccf03
Alexandre Sobota <alexandre.sobota@hotmail.com>
2024-06-18 08:45:34 -0300

    loyaltyCardShow ready

--- src/modules/form/perfilLoad.js
+++ src/modules/form/perfilLoad.js
@@ -1,5 +1,6 @@
 import { perfilFetchByCard } from "../../services/perfilFetchByCard.js";
 import { informationShow } from "../sections/informationShow.js";
+import { loyaltyCardShow } from "../sections/loyaltyCardShow.js";
 
 const form = document.querySelector("form");
 const cardId = document.getElementById("card-id");
@@ -17,6 +18,7 @@ form.onsubmit = async (event) => {
     const information = await perfilFetchByCard(id);
 
     informationShow(information);
+    loyaltyCardShow(information);
   } catch (error) {
     alert("Não foi possível buscar os dados para esse ID!");
     console.log(error);
---
+++ src/modules/sections/loyaltyCardShow.js
@@ -0,0 +1,44 @@
+const h2 = document.querySelector("#loyalty-card header #id");
+// const div = document.querySelectorAll("#loyalty-card .box-checked .checked");
+const boxChecked = document.querySelector("#loyalty-card .box-checked");
+// const items = document.querySelectorAll("#loyalty-card .box-checked .checked");
+
+export function loyaltyCardShow(information) {
+  h2.innerHTML = "";
+  boxChecked.innerHTML = "";
+
+  h2.textContent = `ID: ${information.id}`;
+
+  const divGift = document.createElement("div");
+  divGift.classList.add("checked-gift");
+
+  const totalCuts = Number(information.loyaltyCard.totalCuts);
+
+  const imgGift = document.createElement("img");
+
+  if (totalCuts === 10) {
+    imgGift.setAttribute("src", "./src/assets/icons/gift-color.svg");
+  } else {
+    imgGift.setAttribute("src", "./src/assets/icons/gift-opacity.svg");
+  }
+
+  imgGift.setAttribute("alt", "Icone de gift");
+
+  for (let item = 0; item < 9; item++) {
+    const div = document.createElement("div");
+    div.classList.add("checked");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", "./src/assets/pin-check.png");
+    img.setAttribute("alt", "Icone de check");
+
+    if (item < totalCuts) {
+      div.append(img);
+      boxChecked.append(div);
+    } else {
+      boxChecked.append(div);
+    }
+    divGift.append(imgGift);
+    boxChecked.append(divGift);
+  }
+}
